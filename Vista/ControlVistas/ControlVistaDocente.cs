e58cfdbf020b69c8ad162808507abe013f92f72a
Karmita <18julio1996@gmail.com>
2018-10-18 11:29:46 -0500

    controlador para los eventos de la vista docentes

---
+++ Vista/ControlVistas/ControlVistaDocente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vista.ControlVistas
+{
+    class ControlVistaDocente
+    {
+        public static int id = 0;
+        public static String opcion = "";
+    }
+}
