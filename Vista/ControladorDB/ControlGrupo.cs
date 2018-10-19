38d3ce110d0c8b3d4656d286599c55f12640016c
Karmita <18julio1996@gmail.com>
2018-10-19 15:18:46 -0500

    modificaciones de grupos

---
+++ Vista/ControladorDB/ControlGrupo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vista.Database;
+using Vista.Modelos;
+
+namespace Vista.ControladorDB
+{
+    public class ControlGrupo
+    {
+        DatabaseGrupo databaseGrupo = new DatabaseGrupo();
+        DataTable TablaAux = null;
+        public Boolean Guardar(ModeloGrupo modeloGrupo)
+        {
+            return databaseGrupo.Guardar(modeloGrupo);
+        }
+        public Boolean Actualizar(ModeloGrupo modeloGrupo)
+        {
+            return databaseGrupo.Actualizar(modeloGrupo);
+        }
+        public Boolean Eliminar(int id)
+        {
+            return databaseGrupo.Eliminar(id);
+        }
+        public DataTable RecuperarTablaAux()
+        {
+            return TablaAux;
+        }
+        public DataTable Consultar(String buscar)
+        {
+            DataTable dt = new DataTable();//DataTable que enviaremos a la vista sin mostrar el id de los docentes
+            dt.Columns.Add("SEMESTRE"); dt.Columns.Add("GRUPO"); dt.Columns.Add("AREA");
+            dt.Columns.Add("ESTADO");
+            SQLiteDataAdapter da = databaseGrupo.Consultar(buscar);
+            TablaAux = new DataTable();
+            da.Fill(TablaAux);
+            for (int i = 0; i < TablaAux.Rows.Count; i++)
+            {
+                DataRow row = TablaAux.Rows[i];
+                dt.Rows.Add(row["semestre"], row["nombre_grupos"], row["area"], row["estado"]);
+            }
+            return dt;
+        }
+    }
+}
---
+++ Vista/Database/DatabaseGrupo.cs
@@ -0,0 +1,116 @@
+﻿using Database;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Vista.Modelos;
+
+namespace Vista.Database
+{
+    public class DatabaseGrupo
+    {
+        SQLiteConnection cn = null;
+        SQLiteDataAdapter da;
+        public Boolean Guardar(ModeloGrupo modeloGrupo)
+        {
+            String query = "insert into grupos (nombre_grupos,semestre,area,estado) values(@nombre,@semestre,@area,@estado)";
+            try
+            {
+                cn = Conexion.Instance.Conectar();
+                if (cn != null)
+                {
+                    cn.Open();
+                    SQLiteCommand cmd = new SQLiteCommand(query, cn);
+
+                    cmd.Parameters.AddWithValue("@nombre", modeloGrupo.Nombre_grupos);
+                    cmd.Parameters.AddWithValue("@semestre", modeloGrupo.Semestre);
+                    cmd.Parameters.AddWithValue("@area", modeloGrupo.Area);
+                    cmd.Parameters.AddWithValue("@estado", modeloGrupo.Estado);
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Mensaje Error: " + e.Message);
+                return false;
+            }
+
+        }
+
+        public Boolean Eliminar(int id)
+        {
+            String query = "delete from grupos where id_grupos=@id";
+            try
+            {
+                cn = Conexion.Instance.Conectar();
+                if (cn != null)
+                {
+                    cn.Open();
+                    SQLiteCommand cmd = new SQLiteCommand(query, cn);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error:" + e.Message);
+                return false;
+            }
+        }
+
+        public Boolean Actualizar(ModeloGrupo modeloGrupo)
+        {
+            String query = "update grupos set nombre_grupos=@nombre,semestre=@semestre,area=@area," +
+                "estado=@estado where id_grupos=@id";
+            try
+            {
+                cn = Conexion.Instance.Conectar();
+                if (cn != null)
+                {
+                    cn.Open();
+                    SQLiteCommand cmd = new SQLiteCommand(query, cn);
+                    cmd.Parameters.AddWithValue("@id", modeloGrupo.Id_grupos);
+                    cmd.Parameters.AddWithValue("@nombre", modeloGrupo.Nombre_grupos);
+                    cmd.Parameters.AddWithValue("@semestre", modeloGrupo.Semestre);
+                    cmd.Parameters.AddWithValue("@area", modeloGrupo.Area);
+                    cmd.Parameters.AddWithValue("@estado", modeloGrupo.Estado);
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error:" + e.Message);
+                return false;
+            }
+        }
+
+        public SQLiteDataAdapter Consultar(String buscar)
+        {
+            cn = Conexion.Instance.Conectar();
+            da = new SQLiteDataAdapter("select * from grupos where nombre_grupos like'%" + buscar + "%' " +
+                " or semestre like'%" + buscar + "%' ", cn);
+            return da;
+        }
+    }
+}
