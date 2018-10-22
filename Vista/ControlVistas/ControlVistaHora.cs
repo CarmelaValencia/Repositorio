3a953451ade81e3c06be734168f12f728b4cfd13
Karmita <18julio1996@gmail.com>
2018-10-21 21:58:54 -0500

    CRUD DE HORARIO SIN VALIDACIONES

---
+++ Vista/ControlVistas/ControlVistaHora.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Vista.ControladorDB;
+using Vista.Modelos;
+using Vista.Vistas;
+
+namespace Vista.ControlVistas
+{
+    public class ControlVistaHora
+    {
+        public static String opcion = "";
+        public static agregarhorario vistaAgregarObj = null;
+        public static DataTable tablaAux = new DataTable();
+        public static horario vistaHorarioObj = null;
+        public static ModeloHora mdloHora = null;
+        static ControlHora controlHora = new ControlHora();
+        public void iniciarHoras(horario horas) {
+            vistaHorarioObj = horas;
+            rellenarTabla();
+            vistaHorarioObj.btn_eliminar.Click += new EventHandler(btn_Delete);
+            vistaHorarioObj.txt_buscar.TextChanged += new EventHandler(buscando);
+        }
+        public static void cargarValores(agregarhorario vistaAgregar)
+        {
+            vistaAgregarObj = vistaAgregar;
+            vistaAgregar.btn_aceptar.Click += new EventHandler(btn_aceptar);
+            vistaAgregar.btn_cancelar.Click += new EventHandler(btn_cancelar);
+            if (opcion.Equals("Registrar"))
+            {
+                mdloHora = new ModeloHora(0, "", "","MATUTINO");
+            }
+            vistaAgregar.txt_nombre_docentes.Text = mdloHora.Inicio;
+            vistaAgregar.txt_apellidos.Text = mdloHora.Fin;
+            vistaAgregar.txt_turno.Text = mdloHora.Tipo;
+        }
+        public static void obtenerSeleccion()
+        {
+            int fila = vistaHorarioObj.lista.CurrentCell.RowIndex;
+            DataRow row = ControlVistaHora.tablaAux.Rows[fila];
+            Int32 valor = Convert.ToInt32(row["id_horas"]);
+            mdloHora = new ModeloHora(valor, row["inicio"].ToString(), row["fin"].ToString(), row["tipo"].ToString());
+        }
+        private static void btn_cancelar(object sender, EventArgs e)
+        {
+            
+        }
+
+        private static void btn_aceptar(object sender, EventArgs e)
+        {
+            String inicio = vistaAgregarObj.txt_nombre_docentes.Text;
+            String fin = vistaAgregarObj.txt_apellidos.Text;
+            String turno = vistaAgregarObj.txt_turno.Text;
+            mdloHora = new ModeloHora(mdloHora.IdHoras, inicio, fin,turno);
+            Console.Write("opcion:" + opcion);
+            if (opcion.Equals("Registrar"))
+            {
+                Console.Write("opcion:"+opcion);
+                if (controlHora.Guardar(mdloHora))
+                {
+                    rellenarTabla();
+                    MessageBox.Show("Registro guardado");
+                }
+                else
+                {
+                    MessageBox.Show("Error al guardar el registro");
+                }
+            }
+            else if (opcion.Equals("Modificar"))
+            {
+                if (controlHora.Actualizar(mdloHora))
+                {
+                    rellenarTabla();
+                    MessageBox.Show("Registro actualizado");
+                }
+                else
+                {
+                    MessageBox.Show("Error al actualizar el registro");
+                }
+            }
+        }
+
+        private void buscando(object sender, EventArgs e)
+        {
+            rellenarTabla();
+        }
+
+        private void btn_Delete(object sender, EventArgs e)
+        {
+            int fila = vistaHorarioObj.lista.CurrentCell.RowIndex;
+            DataRow row = tablaAux.Rows[fila];
+            Int32 valor = Convert.ToInt32(row["id_horas"]);
+            if (controlHora.Eliminar(valor))
+            {
+                rellenarTabla();
+                MessageBox.Show("Registro eliminado");
+            }
+            else
+            {
+                MessageBox.Show("Error al eliminar el registro");
+            }
+        }
+
+        private static void rellenarTabla()
+        {
+            vistaHorarioObj.lista.DataSource = controlHora.Consultar(vistaHorarioObj.txt_buscar.Text);
+            tablaAux = controlHora.RecuperarTablaAux();
+        }
+    }
+}
--- Vista/Database/DatabaseHora.cs
+++ Vista/Database/DatabaseHora.cs
@@ -1,13 +1,110 @@
-﻿using System;
+﻿using Database;
+using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using Vista.Modelos;
 
 namespace Vista.Database
 {
     class DatabaseHora
     {
+        SQLiteConnection cn = null;
+        SQLiteDataAdapter da = null;
+        public Boolean Guardar(ModeloHora modeloHora)
+        {
+            String query = "insert into horas (inicio,fin,tipo) values(@inicio,@fin,@tipo)";
+            try
+            {
+                cn = Conexion.Instance.Conectar();
+                if (cn != null)
+                {
+                    cn.Open();
+                    SQLiteCommand cmd = new SQLiteCommand(query, cn);
 
+                    cmd.Parameters.AddWithValue("@inicio", modeloHora.Inicio);
+                    cmd.Parameters.AddWithValue("@fin", modeloHora.Fin);
+                    cmd.Parameters.AddWithValue("@tipo", modeloHora.Tipo);
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Mensaje Error: " + e.Message);
+                return false;
+            }
+        }
+        public Boolean Eliminar(int id)
+        {
+            String query = "delete from horas where id_horas=@id";
+            try
+            {
+                cn = Conexion.Instance.Conectar();
+                if (cn != null)
+                {
+                    cn.Open();
+                    SQLiteCommand cmd = new SQLiteCommand(query, cn);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error:" + e.Message);
+                return false;
+            }
+        }
+        public Boolean Actualizar(ModeloHora modeloHora)
+        {
+            String query = "update horas set inicio=@inicio,fin=@fin,tipo=@turno " +
+                " where id_horas=@id";
+            try
+            {
+                cn = Conexion.Instance.Conectar();
+                if (cn != null)
+                {
+                    cn.Open();
+                    SQLiteCommand cmd = new SQLiteCommand(query, cn);
+                    cmd.Parameters.AddWithValue("@id", modeloHora.IdHoras);
+                    cmd.Parameters.AddWithValue("@inicio", modeloHora.Inicio);
+                    cmd.Parameters.AddWithValue("@fin", modeloHora.Fin);
+                    cmd.Parameters.AddWithValue("@turno", modeloHora.Tipo);
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error:" + e.Message);
+                return false;
+            }
+        }
+        public SQLiteDataAdapter Consultar(String buscar)
+        {
+            cn = Conexion.Instance.Conectar();
+            da = new SQLiteDataAdapter("select * from horas where inicio like'%" + buscar + "%' " +
+                " or fin like'%" + buscar + "%' or tipo like'%" + buscar + "%'", cn);
+            return da;
+        }
     }
 }
